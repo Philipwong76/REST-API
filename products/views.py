d198506066b434f8e21301c925d6243fefbb8153
Philip Wong <110500922+ThatBoiisback@users.noreply.github.com>
2022-11-27 18:03:38 -0500

    Registered Product modle with admin site + Completed POST functionality for Product + Updated Rest Framework decimal settings

--- products/views.py
+++ products/views.py
@@ -1,7 +1,18 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .Serializers import ProductSerializer
+from .models import Product
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def products_list(request):
 
-    return Response('OK')
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
--- products_project/settings.py
+++ products_project/settings.py
@@ -66,6 +66,10 @@ TEMPLATES = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING':False
+}
+
 WSGI_APPLICATION = 'products_project.wsgi.application'
 
 
